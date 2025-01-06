8d0383a40460873532f54bad530ed3e1be8e52b1
unknown <DeezIsAme>
2025-01-06 11:25:59 +0700

    testing onscroll animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 // autoplay mp3 onload website
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show"); // Ganti id menjadi "show"
+    } else {
+      entry.target.classList.remove("show"); // Ganti id menjadi "hidden"
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
 function playAudio() {
   const audio = document.getElementById("background-audio");
   audio.play();
